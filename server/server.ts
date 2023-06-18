6bbf6de57f633632d291dfea89e4cfa4140e7ce4
Euan Cunningham <euancc06@gmail.com>
2023-06-19 11:16:17 +1200

    server.ts edit

--- server/server.ts
+++ server/server.ts
@@ -12,9 +12,9 @@ server.use(express.static(join(__dirname, './public')))
 server.use('/api/vi/locations', locationRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  server.use('/assets', express.static(resolve(__dirname, '../assets')))
+  server.use('/assets', express.static(resolve(__dirname, '../../assets')))
   server.get('*', (req, res) => {
-    res.sendFile(resolve(__dirname, '../index.html'))
+    res.sendFile(resolve(__dirname, '../../index.html'))
   })
 }
 export default server
