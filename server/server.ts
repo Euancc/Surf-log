dea863f6bbcc64aa4f6d263b0bf9184d965c453e
Euan Cunningham <euancc06@gmail.com>
2023-06-19 11:50:43 +1200

    remove server file

--- server/server.ts
+++ server/server.ts
@@ -12,10 +12,10 @@ server.use(express.static(join(__dirname, './public')))
 server.use('/api/vi/locations', locationRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  server.use('/assets', express.static(resolve(__dirname, '../../assets')))
-  server.use('/images', express.static(resolve(__dirname, '../../images')))
+  server.use('/assets', express.static(resolve(__dirname, '../assets')))
+  server.use('/images', express.static(resolve(__dirname, '../images')))
   server.get('*', (req, res) => {
-    res.sendFile(resolve(__dirname, '../../index.html'))
+    res.sendFile(resolve(__dirname, '../index.html'))
   })
 }
 export default server
