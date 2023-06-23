e0fd5fc008d57fb115a815ad6f57f1b58d30f0f0
Euan Cunningham <euancc06@gmail.com>
2023-06-23 16:04:52 +1200

    edit table

--- client/components/TablePage.tsx
+++ client/components/TablePage.tsx
@@ -1,10 +1,30 @@
 import Header from './Header'
 import Footer from './Footer'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { log } from 'console'
+
+const titles = [
+  'Wave face size',
+  'Swell Period',
+  'Swell Direction',
+  'Wind Direction',
+  'Wind Speed',
+  'Last tide',
+  'Time Surfed',
+  'Next tide',
+  'Overall',
+]
 
 export default function TablePage() {
   const { location } = useParams()
   console.log(location)
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  const handleEditClick = (index: number) => {
+    setSelectedIndex(index)
+  }
+  console.log(selectedIndex)
 
   // Change input fields to forms when being edited
   return (
@@ -14,97 +34,77 @@ export default function TablePage() {
         <table>
           <tbody>
             <tr>
-              <th>dd/mm/yyyy</th>
+              <th>
+                dd/mm/yyyy<button>Edit</button>
+              </th>
               <td>
                 <strong>Forecast</strong>
               </td>
               <td>
                 <strong>Actual</strong>
               </td>
-              <td>
-                <strong>
-                  <button>Edit</button>
-                </strong>
-              </td>
             </tr>
-            <tr>
-              <th>
-                <strong>Wave face size</strong>
-              </th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Swell Period</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Swell Direction</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Wind Direction</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Wind Speed</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Last tide</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Time Surfed</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Next tide</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <th>Overall</th>
-              <td>input</td>
-              <td>input</td>
-              <td>
-                <button>Edit</button>
-              </td>
-            </tr>
+            {titles.map((title, index) => {
+              return (
+                <TableRow
+                  onEditClick={handleEditClick}
+                  key={index}
+                  title={title}
+                  isEditing={index === selectedIndex}
+                  index={index}
+                />
+              )
+            })}
           </tbody>
         </table>
       </div>
       <Footer />
     </div>
+  )
+}
+
+interface TableRowProps {
+  title: string
+  index: number
+  isEditing: boolean
+  onEditClick: (index: number) => void
+}
+
+function TableRow({ title, index, onEditClick, isEditing }: TableRowProps) {
+  const [forecastText, setForecastText] = useState('')
+  const [actualText, setactualText] = useState('')
+
+  const handleEditClick = () => {
+    onEditClick(index)
+  }
+
+  return isEditing ? (
+    <tr>
+      <th>
+        <b>{title}</b>
+      </th>
+      <td>
+        <input
+          type="text"
+          value={forecastText}
+          onChange={(event) => setForecastText(event.target.value)}
+        />
+      </td>
+      <td>input</td>
+      <td>
+        <button onClick={handleEditClick}>Edit</button>
+      </td>
+    </tr>
+  ) : (
+    <tr>
+      <th>
+        <b>{title}</b>
+      </th>
+      <td>{forecastText}</td>
+      <td>input</td>
+      <td>
+        <button onClick={handleEditClick}>Edit</button>
+      </td>
+    </tr>
   )
 }
